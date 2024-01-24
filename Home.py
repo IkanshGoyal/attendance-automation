ba61625246d19795ceab7a79ae898810955fe3aa
IkanshGoyal <63862241+IkanshGoyal@users.noreply.github.com>
2024-01-24 11:25:49 +0530

    initial commit

---
+++ Home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("Attendance Web App")
+st.divider()
+
+st.subheader(":blue[Pages:]")
+st.write("Home Page - For navigation")
+st.write("Attendance Page - To view attendance")
+st.write("Login Page - To log in and mark attendance")
+st.write("Sign-Up Page - To register a user")
+st.write("Users Page - To view all logged users")
+st.divider()
+
+st.subheader(":blue[Frameworks and Technologies used:]")
+st.write('OpenCV')
+st.write('json and csv files')
+st.write('numpy and pandas')
+st.write('streamlit')
+st.divider()
+
+st.write("Developed by :blue[Ikansh Goyal]")
+st.write("Section - A")
+st.write("Class Roll Number - 28")
+st.write("University Roll Number - 2021237")
+st.write("B.Tech CSE")
+st.divider()
